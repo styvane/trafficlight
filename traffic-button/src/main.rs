bdd8517cf86b103ef1ae9d017f428b3a801fbc62
Styvane Soukossi <mstyvane@gmail.com>
2021-04-14 23:04:05 +0200

    simplify infinity loop

--- traffic-button/src/main.rs
+++ traffic-button/src/main.rs
@@ -4,11 +4,5 @@ use structopt::StructOpt;
 fn main() {
     let opt = button::ButtonOption::from_args();
     let mut btn = button::Button::new(opt);
-    loop {
-        if let Ok(_) = btn.push() {
-            continue;
-        } else {
-            break;
-        }
-    }
+    while let Ok(_) = btn.push() {}
 }
